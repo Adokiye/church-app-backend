8819fa5ee1da3cdbe955e644d2710df6c874a4db
Iruene Adokiye <adokiye.iruene@mkobo.com.ng>
2021-02-15 17:05:53 +0100

    Addition of knex.js file

---
+++ knexfile.js
@@ -0,0 +1,46 @@
+// Update with your config settings.
+
+module.exports = {
+  staging: {
+    client: 'pg',
+    connection: {
+      host : process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      user:     process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations',
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host : process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      user:     process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations',
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  }
+
+};
