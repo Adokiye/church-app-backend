b0f70ccb41f30bcf46c9518258d6c90f1a78e645
Adokiye <gorgeceo@gmail.com>
2021-04-23 07:04:23 +0100

    to bec hanges

--- src/app/controllers/cokitchen.controller.js
+++ src/app/controllers/cokitchen.controller.js
@@ -116,7 +116,7 @@ export const deleteCokitchenPolygon = async ctx => {
 
 export const getAllCokitchens = async ctx => {
   const cokitchens = await Cokitchen.query().withGraphFetched(
-    '[brands,meals,cokitchen_explore_keywords]'
+    '[brands,meals,]'
   )
   return {
     status: 'success',
