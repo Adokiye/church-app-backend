3eb0763049b3eea441787b680cefe06e8079843c
Adokiye <gorgeceo@gmail.com>
2021-04-23 07:41:07 +0100

    new changes

--- src/app/controllers/cokitchen.controller.js
+++ src/app/controllers/cokitchen.controller.js
@@ -118,7 +118,10 @@ export const getAllCokitchens = async ctx => {
   const cokitchens = await Cokitchen.query()
   .withGraphFetched(
     '[brands,meals,cokitchen_explore_keywords]'
-  )
+  ).catch((e)=>{
+    console.log(e)
+    return []
+  })
   return {
     status: 'success',
     cokitchens
