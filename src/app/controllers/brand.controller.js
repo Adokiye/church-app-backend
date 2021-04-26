34a3e19cd52f315b50b6d8970c066dacef77bf8f
Adokiye <gorgeceo@gmail.com>
2021-04-26 20:30:58 +0100

    new fix

--- src/app/controllers/brand.controller.js
+++ src/app/controllers/brand.controller.js
@@ -85,7 +85,7 @@ export const getBrandsForCustomer = async ctx => {
           .where('cokitchens.id', cokitchen_polygons[i].cokitchen_id)
 
           .withGraphJoined(
-            '[brands.[meals.[meal_category.[meal_category_selection_type(selectNameAndId)]]],cokitchen_explore_keywords.[meal_keyword], cokitchen_polygons]'
+            '[brands.[meals.[addons.[meal_data.[meal_category]], meal_category.[meal_category_selection_type(selectNameAndId)]]],cokitchen_explore_keywords.[meal_keyword], cokitchen_polygons]'
           )
 
           .where('brands:meals.is_addon', false)
