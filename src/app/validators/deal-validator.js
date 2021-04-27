31dab9509553cefd83b9dec9dd438546a58612d7
Adokiye <gorgeceo@gmail.com>
2021-04-27 22:39:56 +0100

    new fixes

--- src/app/validators/deal-validator.js
+++ src/app/validators/deal-validator.js
@@ -4,31 +4,43 @@ import { validationMiddleware } from '../middlewares'
 
 let image = Joi.string()
 
+let brand = Joi.object().keys()
+
 const DealsValidator = {
   createDeal: () =>
     validationMiddleware({
       body: {
         deal_type_id: Joi.string().required(),
-        brands: Joi.array().items(Joi.string()),
+        brands: Joi.array().items(
+          Joi.object().keys({
+            name: Joi.string().required(),
+            id: Joi.string().required()
+          })
+        ),
         name: Joi.string().required(),
         description: Joi.string().required(),
         images: Joi.array().items(image).required(),
         rate: Joi.number().required(),
         min: Joi.string(),
-        to_expire:Joi.date()
+        to_expire: Joi.date()
       }
     }),
   updateDeal: () =>
     validationMiddleware({
       body: {
         deal_type_id: Joi.string(),
-        brands: Joi.array().items(Joi.string()),
+        brands: Joi.array().items(
+          Joi.object().keys({
+            name: Joi.string().required(),
+            id: Joi.string().required()
+          })
+        ),
         name: Joi.string(),
         description: Joi.string(),
         images: Joi.array().items(image),
         rate: Joi.number().required(),
         min: Joi.string(),
-        to_expire:Joi.date()
+        to_expire: Joi.date()
       }
     })
 }
