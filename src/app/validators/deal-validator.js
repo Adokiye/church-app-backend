e35a59c083c369cb763a6faeca1d6f25d1c3c553
Adokiye <gorgeceo@gmail.com>
2021-04-28 11:43:38 +0100

    major dbchange

--- src/app/validators/deal-validator.js
+++ src/app/validators/deal-validator.js
@@ -23,6 +23,8 @@ const DealsValidator = {
         ),
         title: Joi.string().required(),
         heading: Joi.string().required(),
+        discount_code: Joi.string().required(),
+
         body: Joi.string().required(),
         images: Joi.array().items(image).required(),
         rate: Joi.number().required(),
@@ -35,25 +37,40 @@ const DealsValidator = {
         to_expire_date: Joi.date().required(),
         to_expire_time: Joi.string().required(),
         to_start_date: Joi.date().required(),
-        to_start_time: Joi.string().required(),
+        to_start_time: Joi.string()
       }
     }),
   updateDeal: () =>
     validationMiddleware({
       body: {
         deal_type_id: Joi.string(),
+        deal_value_type_id: Joi.string(),
+        deal_requirement_type_id: Joi.string(),
+        deal_eligibility_type_id: Joi.string(),
+
         brands: Joi.array().items(
           Joi.object().keys({
             name: Joi.string().required(),
             id: Joi.string().required()
           })
         ),
-        name: Joi.string(),
-        description: Joi.string(),
+        title: Joi.string(),
+        heading: Joi.string(),
+        discount_code: Joi.string(),
+
+        body: Joi.string(),
         images: Joi.array().items(image),
-        rate: Joi.number().required(),
-        min: Joi.string(),
-        to_expire: Joi.date()
+        rate: Joi.number(),
+        min_amount: Joi.string(),
+        max_amount: Joi.string(),
+
+        min_items: Joi.string(),
+        max_items: Joi.string(),
+
+        to_expire_date: Joi.date(),
+        to_expire_time: Joi.string(),
+        to_start_date: Joi.date(),
+        to_start_time: Joi.string()
       }
     })
 }
