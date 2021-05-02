32d25695f40600c8b01e55c8984efd6f57ab4776
Adokiye <gorgeceo@gmail.com>
2021-05-02 04:39:20 +0100

    new changes

--- src/app/models/calculated_order.js
+++ src/app/models/calculated_order.js
@@ -1,6 +1,6 @@
 import { baseModel, modelUuid } from './index'
 import { Model } from 'objection'
-
+import path from 'path'
 
 class CalculatedOrder extends modelUuid(baseModel) {
   static tableName = 'calculated_orders'
