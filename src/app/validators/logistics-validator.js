7da1ccd6a28aa31f98ac07dd16e54918372ae81c
Adokiye <gorgeceo@gmail.com>
2021-06-03 20:57:55 +0100

    rider accept order

--- src/app/validators/logistics-validator.js
+++ src/app/validators/logistics-validator.js
@@ -15,32 +15,38 @@ const LogisticsValidator = {
   createLogisticsStaff: () =>
     validationMiddleware({
       body: {
-        username: Joi.string().required(),
         first_name: Joi.string().required(),
         last_name: Joi.string().required(),
         other_name: Joi.string(),
-        email: Joi.string().email(),
+        dob: Joi.date(),
+        user_gender: Joi.string().valid('male', 'female'),
+        email: Joi.string().email().required(),
         password: Joi.string()
           .pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$'))
           .message(
             'Password must contain minimun 8 characters, at least 1 UPPERCASE letter, at least 1 lowercase letter and at least 1 digit'
-          ),
+          )
+          .required(),
+        phone_number: Joi.string().min(11).max(11).required(),
         logistics_company_id: Joi.string().required()
       }
     }),
   createLogisticsSuperAdmin: () =>
     validationMiddleware({
       body: {
-        username: Joi.string().required(),
         first_name: Joi.string().required(),
         last_name: Joi.string().required(),
         other_name: Joi.string(),
-        email: Joi.string().email(),
+        dob: Joi.date(),
+        user_gender: Joi.string().valid('male', 'female'),
+        email: Joi.string().email().required(),
         password: Joi.string()
           .pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$'))
           .message(
             'Password must contain minimun 8 characters, at least 1 UPPERCASE letter, at least 1 lowercase letter and at least 1 digit'
           )
+          .required(),
+        phone_number: Joi.string().min(11).max(11).required()
       }
     })
 }
