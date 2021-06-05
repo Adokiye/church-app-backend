ea4cd38a6b02de00e31d54568b7726614680de3a
Adokiye <gorgeceo@gmail.com>
2021-06-05 18:48:38 +0100

    rider accept order

--- src/app/controllers/paystack.controller.js
+++ src/app/controllers/paystack.controller.js
@@ -1,4 +1,4 @@
-import PaystackService from '../services/PaystackService'
+import {handle} from '../services/PaystackService'
 
 
 export const handleCharge = async ctx => {
@@ -8,7 +8,7 @@ export const handleCharge = async ctx => {
   
   switch (event) {
     case 'charge.success':
-      await PaystackService.HandleChargeSuccess.handle(data)
+      await handle(data)
       break
 
     /* istanbul ignore next */
