b16ce28bfac6bd2ac1516d390bceb9668751399b
Adokiye <gorgeceo@gmail.com>
2021-06-06 00:23:57 +0100

    rider accept order

--- src/app/controllers/user_card.controller.js
+++ src/app/controllers/user_card.controller.js
@@ -68,8 +68,57 @@ export const chargeCardForWallet = async ctx => {
       console.log(e)
       throw UnprocessableEntity('User card not updated successfully')
     })
-    return {
-      status: 'success',
-      message: 'Card Charged Successfully',
-    }
+  return {
+    status: 'success',
+    message: 'Card Charged Successfully'
+  }
+}
+
+export const chargeCardForOrder = async ctx => {
+  const { body } = ctx.request
+  const { id } = ctx.state.user.user
+  const { card_id, amount } = body
+
+  const [card, user] = Promise.all([
+    UserCard.query()
+      .findById(card_id)
+      .catch(e => {
+        console.log(e)
+        throw NotFound('Card Not Found')
+      }),
+    User.query()
+      .findById(card_id)
+      .catch(e => {
+        console.log(e)
+        throw NotFound('User Not Found')
+      })
+  ])
+
+  const chargeData = {
+    authorization_code: card.auth,
+    email: user.email,
+    amount
+  }
+
+  const data = await chargeCard(chargeData)
+
+  await createTransactionForWallet(
+    'Deposit',
+    'Credit',
+    amount,
+    user.id,
+    `Deposit of ₦${data.amount} By Card`,
+    `Deposit of ₦${data.amount} By Card`
+  )
+  let card_to_update = await UserCard.query()
+    .patchAndFetchById(card_id, {
+      auth: data.authorization.authorization_code,
+      signature: data.authorization.signature,
+      reusable: data.authorization.reusable
+    })
+    .catch(e => {
+      console.log(e)
+      throw UnprocessableEntity('User card not updated successfully')
+    })
+  return next()
 }
